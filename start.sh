22556372842a7791c9896011690dc5618fd035f4
gestanestle <gesta.nestleo@gmail.com>
2024-05-24 09:26:58 +0800

    chore: push modified v

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-kubectl apply -f ./event-command-service
+kubectl apply -f \
+	https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.16/releases/cnpg-1.16.5.yaml
+kubectl apply -f ./event-command-service -n cnpg-system
