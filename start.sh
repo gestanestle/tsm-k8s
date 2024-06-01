e952afb6f1f4e8880ec71054a181d4283aad2668
gestanestle <gesta.nestleo@gmail.com>
2024-06-01 18:14:20 +0800

    feat: working kafka

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# kubectl apply -f \
-#	https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.16/releases/cnpg-1.16.5.yaml
-kubectl apply --server-side -f \
-	https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.23/releases/cnpg-1.23.1.yaml
-kubectl apply -f ./event-command-service
+kubectl create namespace kafka
+kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
+kubectl apply -f ./kafka -n kafka
+kubectl wait kafka/tsm-cluster --for=condition=Ready --timeout=300s -n kafka
+
+#kubectl apply --server-side -f \
+#	https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.23/releases/cnpg-1.23.1.yaml
+#kubectl apply -f ./event-command-service  -n tsm
--- stop.sh
+++ stop.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-kubectl delete all --all -n default
-kubectl delete cluster ecs-pg-cluster
-kubectl delete configmap ecs-app-config
-kubectl delete secret ecs-psql-user-secret ecs-psql-superuser-secret
+kubectl delete all --all -n tsm
+kubectl delete cluster --all -n tsm
+kubectl delete configmap --all -n tsm
+kubectl delete secret --all -n tsm
+
+kubectl delete namespace kafka
+kubectl delete pvc --all -n kafka
+kubectl delete cluster --all
+kubectl delete configmap --all
+kubectl delete secret --all
