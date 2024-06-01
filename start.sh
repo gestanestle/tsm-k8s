ac20766b12e7feb470753461a2758fe8e9c87ae9
gestanestle <gesta.nestleo@gmail.com>
2024-06-01 20:20:32 +0800

    chore: push debezium files

--- start.sh
+++ start.sh
@@ -3,8 +3,11 @@
 kubectl create namespace kafka
 kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
 kubectl apply -f ./kafka -n kafka
-kubectl wait kafka/tsm-cluster --for=condition=Ready --timeout=300s -n kafka
+kubectl wait kafka/kafka-cluster --for=condition=Ready --timeout=300s -n kafka
 
-#kubectl apply --server-side -f \
-#	https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.23/releases/cnpg-1.23.1.yaml
-#kubectl apply -f ./event-command-service  -n tsm
+kubectl apply --server-side -f \
+	https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.23/releases/cnpg-1.23.1.yaml
+
+kubectl create namespace tsm
+kubectl apply -f ./event-command-service -n tsm
+kubectl wait tsm/ecs-pg-cluster --timeout=300s -n tsm
--- stop.sh
+++ stop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 kubectl delete all --all -n tsm
-kubectl delete cluster --all -n tsm
+
 kubectl delete configmap --all -n tsm
 kubectl delete secret --all -n tsm
 
 kubectl delete namespace kafka
 kubectl delete pvc --all -n kafka
-kubectl delete cluster --all
+
 kubectl delete configmap --all
 kubectl delete secret --all
